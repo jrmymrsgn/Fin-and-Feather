7255c2099530369980d6c5a36433ec6a75656b85
Jeremy Rivera <jrmymrsgn6@gmail.com>
2026-04-18 10:08:04 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 // REPLACE THIS WITH YOUR ACTUAL FIREBASE CONFIG
 const firebaseConfig = {
-    apiKey: "AIzaSyCXQopu0o9BMSp3Dr62QnR_FLw6dMGryC0",
-    authDomain: "fin-and-feather-feeding-3e5c3.firebaseapp.com",
-    projectId: "fin-and-feather-feeding-3e5c3",
-    storageBucket: "fin-and-feather-feeding-3e5c3.firebasestorage.app",
-    messagingSenderId: "99913608949",
-    appId: "1:99913608949:web:8e3b0f65445b8a73539fbc"
+apiKey: "AIzaSyCXQopu0o9BMSp3Dr62QnR_FLw6dMGryC0",
+  authDomain: "fin-and-feather-feeding-3e5c3.firebaseapp.com",
+  databaseURL: "https://fin-and-feather-feeding-3e5c3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fin-and-feather-feeding-3e5c3",
+  storageBucket: "fin-and-feather-feeding-3e5c3.firebasestorage.app",
+  messagingSenderId: "99913608949",
+  appId: "1:99913608949:web:7a9dfe18c68bbbea539fbc"
 };
 
 // Initialize Firebase
