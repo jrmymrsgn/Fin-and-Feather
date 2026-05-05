b5a7d4db45d6f880a9cb8738f5ca0976fb90a6d8
Jeremy Rivera <jrmymrsgn6@gmail.com>
2026-05-05 20:58:18 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,5 @@
-// Your web app's Firebase configuration
-// REPLACE THIS WITH YOUR ACTUAL FIREBASE CONFIG
+// Our web app's Firebase configuration
+// REPLACE THIS WITH ACTUAL FIREBASE CONFIG
 const firebaseConfig = {
     apiKey: "AIzaSyCXQopu0o9BMSp3Dr62QnR_FLw6dMGryC0",
   authDomain: "fin-and-feather-feeding-3e5c3.firebaseapp.com",
