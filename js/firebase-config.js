07ac96906558958eddc3d7fa3b196fd91d7cd538
Jeremy Rivera <jrmymrsgn6@gmail.com>
2026-05-20 00:29:46 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
 // Our web app's Firebase configuration
 // REPLACE THIS WITH ACTUAL FIREBASE CONFIG
 const firebaseConfig = {
-   apiKey: "AIzaSyCXQopu0o9BMSp3Dr62QnR_FLw6dMGryC0",
+    apiKey: "AIzaSyCXQopu0o9BMSp3Dr62QnR_FLw6dMGryC0",
   authDomain: "fin-and-feather-feeding-3e5c3.firebaseapp.com",
   databaseURL: "https://fin-and-feather-feeding-3e5c3-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "fin-and-feather-feeding-3e5c3",
